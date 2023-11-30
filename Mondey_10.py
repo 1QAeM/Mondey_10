939c745ae5ea578e91d4b4b306db7cfe699bb5ba
Aleksandr <106984363+1QAeM@users.noreply.github.com>
2023-11-30 11:47:58 -0800

    Update Mondey_10.py

--- Mondey_10.py
+++ Mondey_10.py
@@ -2,3 +2,7 @@
 # what happend?....add your line
 M = 10 # why not)
 # Let it be Let it be Let it be Letit Be...
+#next day of search and research....sadness...
+# SymbList = ['$','!','&','@','"',';)',':(','#',':/','%','€','£','¥']]
+# AlphList = ['a','b','c','d','e','f']
+#   
