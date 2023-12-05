d3ec4343b61296beb237dcc3ba28e63e42755fd0
Aleksandr <106984363+1QAeM@users.noreply.github.com>
2023-12-05 11:03:45 -0800

    Update Mondey_10.py

--- Mondey_10.py
+++ Mondey_10.py
@@ -5,4 +5,7 @@ M = 10 # why not)
 #next day of search and research....sadness...
 # SymbList = ['$','!','&','@','"',';)',':(','#',':/','%','€','£','¥']]
 # AlphList = ['a','b','c','d','e','f']
-#   
+# next day)...want wake up and jump into coding...but
+# NumList = ['0','1','2','3','4,'5','6','7','8','9']
+# even when i creating lists..I feel like I'm part of the programming community!)))Cool
+# 
