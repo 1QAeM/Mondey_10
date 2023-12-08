1f9c4a24ce7cfbfdbf764bb42e5ad2852bf93fca
Aleksandr <106984363+1QAeM@users.noreply.github.com>
2023-12-08 08:20:34 -0800

    Update Mondey_10.py

--- Mondey_10.py
+++ Mondey_10.py
@@ -8,4 +8,6 @@ M = 10 # why not)
 # next day)...want wake up and jump into coding...but
 # NumList = ['0','1','2','3','4,'5','6','7','8','9']
 # even when i creating lists..I feel like I'm part of the programming community!)))Cool
+# next day...or better day= day + 1... day += 1
+# little step for human but big step for humanity
 # 
